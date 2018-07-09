db656ef63d48dc59a2ad3aae57eecbf12dae66d0
MT <m.truderung@googlemail.com>
2018-07-09 13:54:43 +0200

    Init

---
+++ src/main/java/com/javasampleapproach/angularjpapostgresql/SpringJpaPostgreSqlAngularJsApplication.java
@@ -0,0 +1,12 @@
+package com.javasampleapproach.angularjpapostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaPostgreSqlAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaPostgreSqlAngularJsApplication.class, args);
+	}
+}
